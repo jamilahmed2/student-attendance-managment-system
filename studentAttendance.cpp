80cdbcf528df7e4a77af30a412edda67d3313ee0
jamilahmed2 <ja0935991@gmail.com>
2024-08-17 02:09:42 +0500

    create status file

--- studentAttendance.cpp
+++ studentAttendance.cpp
@@ -9,25 +9,52 @@ int main() {
     int totalStd = 10;
     int totalAbsent = 0;
 
+
+    // Open a file to store the data
+    ofstream outFile("StudentAttendance.txt", ios::app); // Open in append mode
+
+    if (!outFile) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+
     for (int i = 0; i < 10; i++) {
+
+        // Taking/input Student name
         cout << i << " Enter Student Name: ";
         cin >> stdName[i];
 
-        cout << "Enter Student Status (P for Present, A for Absent): ";
+        // Taking/ipnut attendance status 
+        cout << "Enter Student Status (p or P for Present, A for Absent): ";
         cin >> AttStatus[i];
 
         if (AttStatus[i] == "p" || AttStatus[i] == "P") {
             totalPresent++;
         }
+
+        // Write the data to the file
+        outFile << "Student Name: " << stdName[i] << ", Status: " << AttStatus[i] << endl;
+    
     }
 
     totalAbsent = totalStd - totalPresent;
 
+    // out put in terminal
     cout << "Total Present: " << totalPresent << endl;
     cout << "Total Absent: " << totalAbsent << endl;
     cout << "Total Students: " << totalStd << endl;
 
-    // Pause the program before exiting
+    // stored attendance in StudentAttendance.txt
+    outFile << "Total Present: " << totalPresent << endl;
+    outFile << "Total Absent: " << totalAbsent << endl;
+    outFile << "Total Students: " << totalStd << endl;
+    outFile << "-----------------------------------" << endl;
+ 
+    // Close the file
+    outFile.close();
+
+    // Pause the program before exiting 
     cout << "Press any key or enter key to exit...";
     cin.ignore(); 
     cin.get();    
